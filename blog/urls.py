a500fd836c184fc98f6abf2be3d41b078aa4a12d
send4ash <send4ash@gmail.com>
2017-09-04 11:22:13 -0400

    Added post list view and template as well as CSS file.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views # views from blog application
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'), # post_list view
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'), # post_detail view
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone #Imports time zone utility; needed for sorting posts
 from .models import Post # Imports Post model
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') # QuerySet for sorting published posts by created date
-    return render(request, 'blog/post_list.html', {'posts': posts})  #Puts together template using posts
+    return render(request, 'blog/post_list.html', {'posts': posts})  #Puts together post list using templates using data saved in "posts"
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk) # Returns 404 error if post not found
+    return render(request, 'blog/post_detail.html', {'post': post}) # Puts together post detail using template and data saved in "post"
